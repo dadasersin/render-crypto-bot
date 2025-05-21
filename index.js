2d55632c3111ed04ab14beb72cf503788980bba5
dadasersin <54889639+dadasersin@users.noreply.github.com>
2025-05-22 01:17:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 app.listen(port, () => console.log(`Sunucu ${port} portunda çalışıyor`));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
