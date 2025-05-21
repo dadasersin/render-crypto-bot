a55ba0224cd0f64486c99c0eb57f639b5a5448db
dadasersin <54889639+dadasersin@users.noreply.github.com>
2025-05-22 02:40:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,4 @@
-settings = {
-    'rsi': [7, 14, 24],
-    'macd': True,
-    'bollinger': True,
-    'ma': [7, 14, 50, 200],
-    'ema': [7, 14, 50, 200],
-    'stoch_rsi': True,
-    'volume': True,
-    'parabolic_sar': True,
-    'ichimoku': True,
-    'atr': True,
-    'supertrend': True,
-    'supertrend_multiplier': 3
-}
+
 settings = {
     'rsi': {'enabled': True, 'periods': [7, 14, 24]},
     'macd': {'enabled': True},
