9c9d914d76a66d0edb8cca675a746b223e73b648
dadasersin <54889639+dadasersin@users.noreply.github.com>
2025-05-22 02:39:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,3 +12,16 @@ settings = {
     'supertrend': True,
     'supertrend_multiplier': 3
 }
+settings = {
+    'rsi': {'enabled': True, 'periods': [7, 14, 24]},
+    'macd': {'enabled': True},
+    'bollinger': {'enabled': True, 'window': 20, 'window_dev': 2},
+    'ma': {'enabled': True, 'periods': [7, 14, 50, 200]},
+    'ema': {'enabled': True, 'periods': [7, 14, 50, 200]},
+    'stoch_rsi': {'enabled': True},
+    'volume': {'enabled': True},
+    'parabolic_sar': {'enabled': True},
+    'ichimoku': {'enabled': True},
+    'atr': {'enabled': True},
+    'supertrend': {'enabled': True, 'multiplier': 3}
+}
