2a6fad6245288659b5c382afad8ed2bf9b707381
dadasersin <54889639+dadasersin@users.noreply.github.com>
2025-05-22 04:13:45 +0300

    Update start_services.py

--- start_services.py
+++ start_services.py
@@ -1,1 +1,16 @@
+import subprocess
+import time
+
+# Streamlit'i arka planda başlat
+streamlit_process = subprocess.Popen(['streamlit', 'run', 'app.py', '--server.port', '8501'])
+
+# Flask API'yi arka planda başlat
+flask_process = subprocess.Popen(['python', 'api.py'])
+
+try:
+    while True:
+        time.sleep(1)  # Sonsuz döngü ile script çalışmaya devam eder
+except KeyboardInterrupt:
+    streamlit_process.terminate()
+    flask_process.terminate()
 
