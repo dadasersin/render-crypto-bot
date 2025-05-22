082941b91bb9ed9eea3c414cbe0041f0bd007ed9
dadasersin <54889639+dadasersin@users.noreply.github.com>
2025-05-22 03:41:06 +0300

    Update generate_signals.py

--- generate_signals.py
+++ generate_signals.py
@@ -1,1 +1,8 @@
+def generate_signals(df):
+    df['signal'] = None
+    # Örnek: RSI 14 30 altı al, 70 üstü sat
+    if 'RSI_14' in df.columns:
+        df.loc[df['RSI_14'] < 30, 'signal'] = 'buy'
+        df.loc[df['RSI_14'] > 70, 'signal'] = 'sell'
+    return df
 
